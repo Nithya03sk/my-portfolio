294d851603d1c534d50c087aa9d6dab69d69545f
nithyashree.s <nithyashree.s@versa-networks.com>
2024-10-08 13:50:17 +0530

    added base to reset all base pat for assests

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/',  // This sets the base path for all assets
   plugins: [react()],
 })
